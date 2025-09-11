f7de356299082e84e2e77aa3fc19c6bf3f8b3cff
Mechalimo <mechalimo1@gmail.com>
2025-09-11 19:02:06 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,7 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.kotlin.gradle.targets.jvm.KotlinJvmTarget
-import java.util.*
 
 plugins {
-    `maven-publish`
-    signing
     kotlin("multiplatform") version "1.9.24"
     kotlin("plugin.serialization") version "1.9.24"
     id("com.github.johnrengelman.shadow") version "8.1.1"
